5700a0ba96145455c4147c940adc56e02e6e04c4
joengenduvel <robby.decosemaeker@telenet.be>
2014-07-08 18:52:11 +0200

    added intial service

---
+++ app/scripts/services/graphdata.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name moodboardApp.graphData
+ * @description
+ * # graphData
+ * Service in the moodboardApp.
+ */
+angular.module('moodboardApp')
+  .service('GraphData', function Graphdata() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
