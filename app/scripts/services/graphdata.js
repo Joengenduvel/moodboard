c385be0ef1c2a1a21d1e164eac640e0c3d695a70
joengenduvel <robby.decosemaeker@telenet.be>
2014-08-23 10:17:02 +0200

    added graph data conversion service

--- app/scripts/services/graphdata.js
+++ app/scripts/services/graphdata.js
@@ -8,8 +8,17 @@
  * Service in the moodboardApp.
  */
 angular.module('moodboardApp')
-  .service('GraphData', function Graphdata() {
+  .service('GraphData', ['$filter', function ($filter) {
     return {
-
+        convert: function(data){
+            var convertedData = [];
+            for(var index = 0; index < data.length; index++){
+                convertedData[index] = {};
+                convertedData[index].x = $filter('date')(data[index].date, 'yyyy-MM-dd');
+                convertedData[index].y = (angular.isUndefined(data[index].happiness))? null: data[index].happiness.value;
+                convertedData[index].index = data[index].index;
+            }
+            return convertedData;
+        }
     };
-  });
+  }]);
--- test/spec/services/graphdata.js
+++ test/spec/services/graphdata.js
@@ -2,16 +2,113 @@
 
 describe('Service: graphData', function () {
 
-  // load the service's module
-  beforeEach(module('moodboardApp'));
+    // load the service's module
+    beforeEach(module('moodboardApp'));
 
-  // instantiate service
-  var graphData;
-  beforeEach(inject(function (_GraphData_) {
-    graphData = _GraphData_;
-  }));
+    // instantiate service
+    var graphData;
+    beforeEach(inject(function (_GraphData_) {
+        graphData = _GraphData_;
+    }));
 
-  it('should do something', function () {
-    expect(!!graphData).toBe(true);
-  });
+    var firebaseData;
+
+    beforeEach(function () {
+        firebaseData = [
+            {
+                comment: "new comment",
+                date: "2014-07-09T17:18:25.989Z",
+                happiness: {
+                    label: "Meh",
+                    value: -2
+                },
+                index: 0
+            },
+            {
+                comment: "new comment",
+                date: "2014-07-10T17:18:25.989Z",
+                happiness: {
+                    label: "Meh",
+                    value: -1
+                },
+                index: 1
+            },
+            {
+                comment: "new comment",
+                date: "2014-07-11T17:18:25.989Z",
+                happiness: {
+                    label: "Meh",
+                    value: 0
+                },
+                index: 2
+            },
+            {
+                comment: "new comment",
+                date: "2014-07-12T17:18:25.989Z",
+                happiness: {
+                    label: "Meh",
+                    value: 1
+                },
+                index: 3
+            },
+            {
+                comment: "new comment",
+                date: "2014-07-13T17:18:25.989Z",
+                happiness: {
+                    label: "Meh",
+                    value: 2
+                },
+                index: 4
+            },
+            {
+                comment: "new comment",
+                date: "2014-07-13T17:18:25.989Z",
+
+                index: 5
+            }
+        ]
+    });
+
+
+    it('should do something', function () {
+        expect(!!graphData).toBe(true);
+    });
+
+    describe('when converting firebase data', function () {
+
+        var convertedData;
+
+        beforeEach(function () {
+            convertedData = graphData.convert(firebaseData);
+        });
+
+
+        it('should convert the date to an x value', function () {
+            expect(convertedData[0].x).toEqual('2014-07-09');
+            expect(convertedData[1].x).toEqual('2014-07-10');
+            expect(convertedData[2].x).toEqual('2014-07-11');
+            expect(convertedData[3].x).toEqual('2014-07-12');
+            expect(convertedData[4].x).toEqual('2014-07-13');
+        });
+
+        it('should convert the happiness to an y value', function(){
+            expect(convertedData[0].y).toEqual(-2);
+            expect(convertedData[1].y).toEqual(-1);
+            expect(convertedData[2].y).toEqual(0);
+            expect(convertedData[3].y).toEqual(1);
+            expect(convertedData[4].y).toEqual(2);
+        });
+
+        it('should preserve the index', function(){
+            expect(convertedData[0].index).toEqual(0);
+            expect(convertedData[1].index).toEqual(1);
+            expect(convertedData[2].index).toEqual(2);
+            expect(convertedData[3].index).toEqual(3);
+            expect(convertedData[4].index).toEqual(4);
+        });
+
+        it('should set unmapped happiness to null', function(){
+            expect(convertedData[5].y).toBe(null);
+        })
+    });
 });
